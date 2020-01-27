9af993947f139068e013a314064400e61cf703c6
Vladislav <vladborisov182@gmail.com>
2020-01-27 17:31:29 +0200

    fix: deleted useless code #35

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,17 +1,3 @@
-from django.conf import settings
-from django.urls import path
-
 from rest_framework.routers import DefaultRouter
 
-from .views import TestApiView
-
 router = DefaultRouter()
-urlpatterns = []
-
-# TODO: remove this test handler as soon as we have actual views to test against
-if settings.ENV in ["test", "local"]:
-    urlpatterns += [
-        path("handler-test/", TestApiView.as_view(), name="handler-test-url")
-    ]
-
-urlpatterns += router.urls
--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,10 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 
+from apps.core.urls import router
+
 urlpatterns = (
-    path(settings.ADMIN_URL, admin.site.urls),
-    path("api/v0/", include(("apps.core.urls", "core"), namespace="v0")),
+    path("admin/", admin.site.urls),
+    path("api/v0/", include((router.urls, "apps.core"), namespace="v0")),
     re_path(r"^django-rq/", include("django_rq.urls")),
 )
