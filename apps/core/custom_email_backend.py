9958bde84528ef1f543e07ac6ff86198c9a643cb
Vladislav <vladborisov182@gmail.com>
2020-01-28 16:02:06 +0200

    feat: added custom email backend #36

--- apps/core/custom_email_backend.py
+++ apps/core/custom_email_backend.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.core.mail.backends.base import BaseEmailBackend
 
-# from django.core.mail.backends.smtp import EmailBackend
+from saasy.client import Client
 
 
 class CustomEmailBackend(BaseEmailBackend):
@@ -8,5 +9,30 @@ class CustomEmailBackend(BaseEmailBackend):
     Custom EmailBackend class for sending emails using the saasy package
     """
 
+    def __init__(self, api_key=None, **kwargs):
+        super().__init__()
+        if not settings.SAASY_API_KEY:
+            raise ValueError(
+                "Set the SAASY_API_KEY in the project"
+                " settings for using CustomEmailBackend"
+            )
+        self.saasy = Client(auth_token=settings.SAASY_API_KEY)
+
     def send_messages(self, email_messages):
-        pass
+        if not email_messages:
+            return False
+
+        for email_message in email_messages:
+            recipients = email_message.recipients()
+            if not recipients:
+                return False
+
+            for recipient in recipients:
+                mail = self.saasy.create_mail(
+                    {
+                        "to_address": recipient,
+                        "context": email_message.body,
+                        "template": email_message.subject,
+                    }
+                )
+                self.saasy.send_mail(mail["id"])
---
+++ apps/core/tests/test_custom_email_backend.py
@@ -0,0 +1,106 @@
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+
+import pytest
+
+from apps.core.custom_email_backend import CustomEmailBackend
+
+DEFAULT_CONTEXT = {
+    "first_context_variable": "Hello",
+    "second_context_variable": "World",
+}
+RECIPIENT_EMAIL = "some@mail.com"
+TEMPLATE_NAME = "some template"
+
+
+def change_project_settings(settings):
+    settings.SAASY_API_KEY = "some_key"
+    settings.EMAIL_BACKEND = "apps.core.custom_email_backend.CustomEmailBackend"
+
+
+def get_mocked_saasy_functions(mocker):
+    mocked_create_mail_func = mocker.patch("saasy.client.Client.create_mail")
+    mocked_create_mail_func.side_effect = lambda x: {"id": 1}
+    mocked_send_mail_func = mocker.patch("saasy.client.Client.send_mail")
+
+    return mocked_create_mail_func, mocked_send_mail_func
+
+
+@pytest.mark.parametrize("email_class", [EmailMultiAlternatives, EmailMessage])
+def test_custom_backend_send_email(settings, mocker, email_class):
+    change_project_settings(settings)
+    mocked_create_mail_func, mocked_send_mail_func = get_mocked_saasy_functions(mocker)
+
+    email_message = email_class(
+        subject=TEMPLATE_NAME, body=DEFAULT_CONTEXT, to=[RECIPIENT_EMAIL]
+    )
+    email_message.send()
+
+    assert mocked_create_mail_func.call_count == 1
+    assert mocked_send_mail_func.call_count == 1
+    assert mocked_create_mail_func.call_args[0][0] == {
+        "to_address": RECIPIENT_EMAIL,
+        "context": DEFAULT_CONTEXT,
+        "template": TEMPLATE_NAME,
+    }
+    assert mocked_send_mail_func.call_args[0][0] == 1
+
+
+@pytest.mark.parametrize("email_class", [EmailMultiAlternatives, EmailMessage])
+def test_custom_email_backend_correct_email_messages(settings, mocker, email_class):
+    change_project_settings(settings)
+    mocked_create_mail_func, mocked_send_mail_func = get_mocked_saasy_functions(mocker)
+
+    email_message = email_class(
+        subject=TEMPLATE_NAME, body=DEFAULT_CONTEXT, to=[RECIPIENT_EMAIL]
+    )
+
+    CustomEmailBackend().send_messages([email_message])
+
+    assert mocked_create_mail_func.call_count == 1
+    assert mocked_send_mail_func.call_count == 1
+    assert mocked_create_mail_func.call_args[0][0] == {
+        "to_address": RECIPIENT_EMAIL,
+        "context": DEFAULT_CONTEXT,
+        "template": TEMPLATE_NAME,
+    }
+    assert mocked_send_mail_func.call_args[0][0] == 1
+
+
+def test_custom_email_backend_without_api_key(settings):
+    settings.EMAIL_BACKEND = "apps.core.custom_email_backend.CustomEmailBackend"
+
+    with pytest.raises(ValueError) as em:
+        email_message = EmailMultiAlternatives(
+            subject=TEMPLATE_NAME, body=DEFAULT_CONTEXT, to=[RECIPIENT_EMAIL]
+        )
+        email_message.send()
+    assert (
+        "Set the SAASY_API_KEY in the project" " settings for using CustomEmailBackend"
+    ) == em.value.args[0]
+
+
+@pytest.mark.parametrize("email_class", [EmailMultiAlternatives, EmailMessage])
+def test_custom_email_backend_messages_without_recipients(
+    settings, mocker, email_class
+):
+    change_project_settings(settings)
+    mocked_create_mail_func, mocked_send_mail_func = get_mocked_saasy_functions(mocker)
+
+    email_message = email_class(subject=TEMPLATE_NAME, body=DEFAULT_CONTEXT)
+
+    backend_response = CustomEmailBackend().send_messages([email_message])
+
+    assert backend_response is False
+    assert mocked_create_mail_func.call_count == 0
+    assert mocked_send_mail_func.call_count == 0
+
+
+def test_custom_email_backend_messages_without_messages(settings, mocker):
+    change_project_settings(settings)
+    mocked_create_mail_func, mocked_send_mail_func = get_mocked_saasy_functions(mocker)
+
+    backend_response = CustomEmailBackend().send_messages([])
+
+    assert backend_response is False
+    assert mocked_create_mail_func.call_count == 0
+    assert mocked_send_mail_func.call_count == 0
