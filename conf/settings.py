33ec1ae749e5454e1d50e6591e62f2efde2cce6f
Vladislav <vladborisov182@gmail.com>
2020-02-05 14:38:49 +0200

    fix: fixed simple jwt settings #30

--- conf/settings.py
+++ conf/settings.py
@@ -280,8 +280,8 @@ REST_AUTH_SERIALIZERS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=60 * 60 * 72),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60 * 60 * 12),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM": "HS256",
@@ -296,8 +296,6 @@ SIMPLE_JWT = {
     "TOKEN_TYPE_CLAIM": "token_type",
     "JTI_CLAIM": "jti",
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
-    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
 ########################################################################################
