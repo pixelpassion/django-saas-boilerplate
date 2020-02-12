1bfc322b5922653ca8981ae77e9821144a23e0ce
Vladislav <vladborisov182@gmail.com>
2020-02-12 17:52:25 +0200

    test: added tests for command #38

--- apps/gdpr/tests/test_send_account_info_command.py
+++ apps/gdpr/tests/test_send_account_info_command.py
@@ -7,14 +7,14 @@ from django.utils import timezone
 import pytest
 
 from apps.core.tests.base_test_utils import mock_email_service_function
+from apps.gdpr.management.commands.send_account_info import (
+    Command as SendAccountInfoCommand,
+)
 
 pytestmark = pytest.mark.django_db
 
 
-def test_send_account_info_command(user_factory, mocker):
-    mocked_delete_email_func = mock_email_service_function(
-        mocker, "send_account_info_is_ready_email"
-    )
+def create_users_with_different_link_data(user_factory):
     # create users with different last_account_info_created
     # dates and account_info_link, account_info_sent values
     users_expired_link = user_factory.create_batch(
@@ -42,6 +42,20 @@ def test_send_account_info_command(user_factory, mocker):
         account_info_sent=True,
     )  # users with sent data
 
+    return users_expired_link, users_expired_link_sent_data, users_valid_link
+
+
+def test_send_account_info_command(user_factory, mocker):
+    mocked_delete_email_func = mock_email_service_function(
+        mocker, "send_account_info_is_ready_email"
+    )
+
+    (
+        users_expired_link,
+        users_expired_link_sent_data,
+        users_valid_link,
+    ) = create_users_with_different_link_data(user_factory)
+
     users_for_link_deletion_count = len(users_expired_link) + len(
         users_expired_link_sent_data
     )
@@ -61,3 +75,21 @@ def test_send_account_info_command(user_factory, mocker):
         assert user.account_info_link is not None
         assert user.last_account_info_created is not None
         assert user.account_info_sent
+
+
+def test_send_account_info_command_functions(user_factory):
+    command = SendAccountInfoCommand()
+    (
+        users_expired_link,
+        users_expired_link_sent_data,
+        users_valid_link,
+    ) = create_users_with_different_link_data(user_factory)
+
+    users_for_link_deletion_count = len(users_expired_link) + len(
+        users_expired_link_sent_data
+    )
+
+    assert command._get_users_for_deletion().count() == users_for_link_deletion_count
+    assert command._get_users_for_send_account_data_email().count() == len(
+        users_valid_link
+    )
