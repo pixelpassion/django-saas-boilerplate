5412fe6dedf2b9d43d82d0a084e92e26be51fbcd
Dmitry <dmitrychevtaev.dev@gmail.com>
2020-03-12 13:12:45 +0200

    tests: added tests for welcomepageview #57

---
+++ apps/core/tests/test_welcome_page_view.py
@@ -0,0 +1,22 @@
+from apps.core.views import WelcomePageView
+
+
+def test_welcome_view_get_context_data(settings):
+    """Test that get_context_data in WelcomePageView correctly returns context."""
+    settings.APP_NAME = "app_name"
+    settings.SENTRY_DSN = None
+    settings.API_URL = None
+    settings.PUBLIC_API_DOCUMENTATION = None
+    view = WelcomePageView()
+    context = view.get_context_data()
+    assert "APP_NAME" in context
+    for name in ["API_URL", "SENTRY_DSN", "PUBLIC_API_DOCUMENTATION"]:
+        assert name not in context
+
+
+def test_welcome_page_view_integration_test(client):
+    """Assert that anonymous client can access WelcomePageView and that
+    welcome.html used as a template"""
+    response = client.get("")
+    assert response.status_code == 200
+    assert "welcome.html" in (t.name for t in response.templates)
