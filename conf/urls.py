d967e39f6211cdcc327457468e523963e8dc7e8a
Dmitry <dmitrychevtaev.dev@gmail.com>
2020-03-12 13:14:27 +0200

    feature: welcomepage #57

--- conf/urls.py
+++ conf/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 
+from apps.core.views import WelcomePageView
+
 urlpatterns = (
     path(settings.ADMIN_URL, admin.site.urls),
     path("api/v0/", include(("apps.core.urls", "core"), namespace="v0")),
     re_path(r"^django-rq/", include("django_rq.urls")),
+    path("", WelcomePageView.as_view()),
 )
