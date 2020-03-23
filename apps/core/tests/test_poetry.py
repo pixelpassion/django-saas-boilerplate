75c124f289668e38fab67cbca851297ecf437c9b
Dmitry <dmitrychevtaev.dev@gmail.com>
2020-03-23 12:56:15 +0200

    test: fixed test_requirements_circleci test #58

--- apps/core/tests/test_poetry.py
+++ apps/core/tests/test_poetry.py
@@ -1,8 +1,21 @@
 import hashlib
 import os
 
+import pytest
 
-def test_requirements_circleci():
+
+@pytest.fixture
+def remove_tempdir(request):
+    """ This fixture remove temporary directory created for the media root
+    """
+
+    def fin():
+        os.remove("temprequirements.txt")
+
+    request.addfinalizer(fin)
+
+
+def test_requirements_circleci(remove_tempdir):
     """Test that generates temprequirements.txt file from pyproject.toml and
        checks if we have the same requirements in our project."""
 
@@ -13,4 +26,3 @@ def test_requirements_circleci():
         hashlib.md5(project_requirements.read().encode("utf-8")).hexdigest()
         == hashlib.md5(test_requirements.read().encode("utf-8")).hexdigest()
     )
-    os.remove("temprequirements.txt")
