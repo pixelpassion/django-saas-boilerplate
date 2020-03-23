2ee5c558cdae97dd7a5b81c50dd832270f2e7c95
Dmitry <dmitrychevtaev.dev@gmail.com>
2020-03-23 13:31:12 +0200

    chore: fixed pyproject.toml #58

--- apps/core/tests/test_poetry.py
+++ apps/core/tests/test_poetry.py
@@ -21,9 +21,10 @@ def test_requirements_circleci(remove_tempdir):
        checks if we have the same requirements in our project."""
 
     os.system("poetry export -f requirements.txt > temprequirements.txt")
-    project_requirements = open("requirements.txt", "r")
-    test_requirements = open("temprequirements.txt", "r")
-    assert (
-        hashlib.md5(project_requirements.read().encode("utf-8")).hexdigest()
-        == hashlib.md5(test_requirements.read().encode("utf-8")).hexdigest()
-    )
+    with open("requirements.txt", "r") as project_requirements, open(
+        "temprequirements.txt", "r"
+    ) as test_requirements:
+        assert (
+            hashlib.md5(project_requirements.read().encode("utf-8")).hexdigest()
+            == hashlib.md5(test_requirements.read().encode("utf-8")).hexdigest()
+        )
